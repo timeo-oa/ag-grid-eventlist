763f54cab106325b7b0f124a1a1435db2e1b4741
Timeo Williams <timeo@outsideanalytics.com>
2021-11-04 12:13:05 -0400

    CSS Styling changes. Still need to resolve Status column width & missing Long location value

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import '@astrouxds/ag-grid-theme/dist/main.css'
 import 'ag-grid-community/dist/styles/ag-grid.css';
- //import 'ag-grid-community/dist/styles/ag-theme-balham.css';
+//import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 import createEvents from './createEvents';
 function App() {
 
@@ -19,81 +19,97 @@ function App() {
       field: 'status',
       sortable: true,
       filter: true,
-      checkboxSelection: true
+      checkboxSelection: false,
+      cellStyle: function (params) {
+        //console.log('Params obj rowHeight is', params.data)
+        if (params.value == 'New') {
+          //Here you can check the value and based on that you can change the color
+          return { color: 'black', backgroundColor: '#FF3838' };
+        } else {
+          return { color: 'black', backgroundColor: 'yellow' };;
+        }
+      }
     },
+
     {
       headerName: 'Hits',
       field: 'hits',
       sortable: true,
       filter: true,
-      checkboxSelection: true
+      checkboxSelection: false
     },
     {
       headerName: 'First Detect',
       field: 'first-detect',
       sortable: true,
       filter: true,
-      checkboxSelection: true
+      checkboxSelection: false
     },
     {
       headerName: 'Duration',
       field: 'duration',
       sortable: true,
       filter: true,
-      checkboxSelection: true
+      checkboxSelection: false
     },
     {
       headerName: 'Location',
       field: 'location',
       sortable: true,
       filter: true,
-      checkboxSelection: true
+      checkboxSelection: false,
+      cellRenderer: function(param){
+        return param.value[0] + '<br/>' + param.value[1];
+      }
     }
   ]
 
   const rowDefs = [];
 
-  const initialState= {
+  const initialState = {
     columnDefs: columnDefs,
     rowData: rowDefs,
   }
 
-  for(let i = 0; i < 1000; i += 1) {
+  for (let i = 0; i < 1000; i += 1) {
     rowDefs[i] = createEvents();
   }
   const [events, setEvents] = useState(initialState);
 
-    var gridOptions = {
-      // PROPERTIES
-      // Objects like myRowData and myColDefs would be created in your application
-      rowData: events.rowData,
-      columnDefs: events.columnDefs,
-      // pagination: true,
-      rowSelection: 'single',
-  
-      // EVENTS
-      // Add event handlers
-      onRowClicked: event => console.log('A row was clicked'),
-      onColumnResized: event => console.log('A column was resized'),
-      onGridReady: event => console.log('The grid is now ready'),
-  
-      // CALLBACKS
-      isScrollLag: () => false
+  var gridOptions = {
+    // PROPERTIES
+    // Objects like RowData and ColDefs would be created in your application
+    rowData: events.rowData,
+    columnDefs: events.columnDefs,
+    animateRows: true,
+    maxWidth: 50,
+    width:50,
+    // pagination: true,
+    rowSelection: 'single',
+    rowHeight: 50,
+    // EVENTS
+    // Add event handlers
+    onRowClicked: event => console.log('A row was clicked'),
+    onColumnResized: event => console.log('A column was resized'),
+    onGridReady: event => console.log('The grid is now ready'),
+
+    // CALLBACKS
+    isScrollLag: () => false
   }
 
 
   return (
     <div
-    className="ag-theme-balham"
-    style={{
-      height: '1000px',
-      width: '1600px'
-    }}
-  >
-    <AgGridReact 
-      gridOptions={gridOptions}
+      className="ag-theme-balham"
+      style={{
+        height: '700px',
+        width: '1600px'
+      }}
+    >
+      <AgGridReact
+        gridOptions={gridOptions}
       ></AgGridReact>
-      </div>
+    </div>
   );
 }
 
